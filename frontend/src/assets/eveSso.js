87d395f9b7c08f66f90887c891c127065346ec08
TheOneNite <travisrilea@gmail.com>
2020-01-22 22:37:01 -0500

    fix: proxy generator bug

--- frontend/src/assets/eveSso.js
+++ frontend/src/assets/eveSso.js
@@ -2,6 +2,6 @@ import proxyBuilder from "./proxy.js";
 
 const redirect = proxyBuilder("redirect-beta");
 
-const uri = `https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=${devURL}/sso-auth&scope=publicData&client_id=c23147201ca9455a841bd6f06df98842`;
+const uri = `https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=${redirect}/sso-auth&scope=publicData&client_id=c23147201ca9455a841bd6f06df98842`;
 
 export { uri };
