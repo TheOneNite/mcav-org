31f3e01384e5bf9b957acffd4d666b102032e400
TheOneNite <travisrilea@gmail.com>
2020-01-22 22:35:41 -0500

    added frontend proxy generator

--- frontend/src/assets/eveSso.js
+++ frontend/src/assets/eveSso.js
@@ -1,4 +1,6 @@
-import devURL from "./proxy.js";
+import proxyBuilder from "./proxy.js";
+
+const redirect = proxyBuilder("redirect-beta");
 
 const uri = `https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=${devURL}/sso-auth&scope=publicData&client_id=c23147201ca9455a841bd6f06df98842`;
 
