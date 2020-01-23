de5af0348604b9ddc0742edf815101a57823decd
TheOneNite <travisrilea@gmail.com>
2020-01-22 22:29:31 -0500

    temp urls

--- frontend/src/assets/proxy.js
+++ frontend/src/assets/proxy.js
@@ -1,4 +1,4 @@
 //const devURL = "http://localhost:4000";
 //const devURL = "http://mcav.org";
-const devURL = "http://serve.compayn.space";
+const devURL = "http://compayn.space:8080";
 export default devURL;
