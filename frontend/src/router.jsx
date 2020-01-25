39ed9d934a2117ac59f1cd2658fdbbf932fc3bc7
TheOneNite <travisrilea@gmail.com>
2020-01-25 00:04:30 -0500

    fix

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import { proxyBuilder } from "./assets/proxy.js";
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 const uri = proxyBuilder("server-beta");
 
